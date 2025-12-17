368453a7b9757709597140806376ff02961ed356
leandro <leandrogit95@gmail.com>
2025-12-17 09:59:00 -0300

    Implementação do Widget Row e Center

---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Adicone uma tarefa',
+                    hintText: 'Estudar Flutter'
+                  ),
+                ),
+              ),
+              SizedBox(width: 8),
+              ElevatedButton(onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff00d7f3),
+                  padding: EdgeInsets.all(16)
+
+                ),
+                child: Icon(
+                 Icons.add,
+                  size: 30,
+                  color: Colors.white
+                ),
+              )
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
